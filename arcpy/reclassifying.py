1a2b776f8cd316cc68e045c2c582cb8dd0dc323c
méch frazier <41482472+mefrazi2@users.noreply.github.com>
2022-04-07 15:16:57 -0500

    reclassifying

---
+++ arcpy/reclassifying.py
@@ -0,0 +1,20 @@
+import arcpy 
+from arc import env
+from arcpy.sa import *
+
+env.workspace = "C:/Users/Desktop/reclass_data"
+in_raster = "C:/Users/Desktop/reclass_data/ebk.tif"
+reclassField = "VALUE"
+
+# first is variable, can be string, second is the int your reclassifying as
+remap = RemapValue ([[0.494846, 1],
+[0.81637, 23379],
+[0.819229, 23380],
+[0.819242, 23381],
+[0.819782, 23382],
+[0.82011, 23383],
+[0.827907, 23384],
+[0.830895, 23385],
+[0.834933, 23386]])
+
+outReclassify = Reclassify(inRaster, reclassField, remap, 'NODATA')
